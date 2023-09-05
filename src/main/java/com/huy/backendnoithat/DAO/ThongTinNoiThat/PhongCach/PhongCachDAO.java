5e6e49e34fe77fa83d29f7ae34395d7c56f77d04
huy <truongvanhuy2000@gmail.com>
2023-09-06 01:01:35 +0700

    change jpa implementation

--- src/main/java/com/huy/backendnoithat/DAO/ThongTinNoiThat/PhongCach/PhongCachDAO.java
+++ src/main/java/com/huy/backendnoithat/DAO/ThongTinNoiThat/PhongCach/PhongCachDAO.java
@@ -1,16 +1,21 @@
 package com.huy.backendnoithat.DAO.ThongTinNoiThat.PhongCach;
 
 import com.huy.backendnoithat.Entity.BangNoiThat.PhongCachNoiThatEntity;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface PhongCachDAO {
-    List<PhongCachNoiThatEntity> findAll();
-    PhongCachNoiThatEntity findById(int id);
-    PhongCachNoiThatEntity findUsingName(String name);
-    void save(PhongCachNoiThatEntity phongCachNoiThatEntity);
-    void deleteById(int id);
-    void update(PhongCachNoiThatEntity phongCachNoiThatEntity);
+public interface PhongCachDAO extends CrudRepository<PhongCachNoiThatEntity, Integer> {
+    @Query("FROM PhongCachNoiThatEntity WHERE name = :name")
+    PhongCachNoiThatEntity findUsingName(@Param("name") String name);
+    @Query("UPDATE PhongCachNoiThatEntity SET name = :name WHERE id = :id")
+    void update(@Param("name") String name,@Param("id") int id);
+    @Query("FROM PhongCachNoiThatEntity p JOIN FETCH p.noiThatEntity")
     List<PhongCachNoiThatEntity> findAllAndJoinFetch();
-    PhongCachNoiThatEntity findByIdAndJoinFetch(int id);
+    @Query("SELECT DISTINCT a FROM PhongCachNoiThatEntity a "
+            + "JOIN FETCH a.noiThatEntity b "
+            + "WHERE a.id = :id")
+    PhongCachNoiThatEntity findByIdAndJoinFetch(@Param("id") int id);
 }
