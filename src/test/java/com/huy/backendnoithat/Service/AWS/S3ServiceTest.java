192fc670799c124c2898239fc6da3d102702a6ba
huy <truongvanhuy2000@gmail.com>
2024-07-07 16:50:21 +0700

    multipart

---
+++ src/test/java/com/huy/backendnoithat/Service/AWS/S3ServiceTest.java
@@ -0,0 +1,16 @@
+package com.huy.backendnoithat.Service.AWS;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+class S3ServiceTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
